016e2745271e518c66e8e8235a1c2c58a7deef22
DESKTOP-DVCUVBK\dika <mahardikaari51@yahoo.com>
2017-03-28 12:27:36 +0700

    success layout

---
+++ success.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+	<title>ThankYou</title>
+	<script src="js/jquery-3.1.1.min.js"></script>
+</head>
+<body>
+    <div class="navbar navbar-default navbar-static-top" style="padding: 2%;">
+      <div class="container">
+      	<center><h1 style="letter-spacing: 5px;">WEB BASED POLLING SYSTEM</h1></center>
+      </div>
+    </div><br><br><br>
+    <div class="container">
+    	<div class="jumbotron animated zoomInUp">
+    		<h2>Terima Kasih Atas Partsipasinya</h2>
+    		<h4>Penghitungan Suara akan dilakukan setelah proses voting berakhir</h4>
+    		<a href="" class="btn btn-default btn-lg">Keluar</a>
+    		<br>
+    		<p align="right">Developed By : Team Z</p>
+    	</div>
+    </div>
+    <div class="navbar navbar-default navbar-fixed-bottom" style="padding: 1%;">
+    	<center><h1 style="letter-spacing: 10px;">Powered By : Team Z</h1></center>
+    </div>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
